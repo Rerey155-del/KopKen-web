e14085991792a63d61effcc14d37e24539f49685
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-11-06 13:59:24 +0700

    okke

--- src/pages/Keranjang.jsx
+++ src/pages/Keranjang.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Add from "../assets/plus.png";
 import Remove from "../assets/remove.png";
 import Delete from "../assets/delete.png";
